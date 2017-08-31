1acd4b710bde384f07f25e75c657cf4eaa29829c
Py-get <jack.harwood.ellenbrook@gmail.com>
2017-08-31 17:27:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
+
+'''from multiprocessing import Queue
+Only needed when building .exe with py-installer'''
+
 url = input("Input url: ")
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
